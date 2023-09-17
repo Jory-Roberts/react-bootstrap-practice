2a9733746b7cfec4ae56813c705309861c8e5e75
Jory Roberts <robertsjory13@gmail.com>
2023-09-16 23:59:42 -0500

    creates NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,18 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './custom.scss';
+import Navigation from './components/Navbar';
 import Form from './components/Form';
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
-        <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <Form />
-            <div className="card">
-                <button
-                    className="btn btn-primary"
-                    onClick={() => setCount((count) => count + 1)}
-                >
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
+        <Router>
+            <Switch>
+                <Route path="/signup" component={Form}></Route>
+                <Navigation />
+            </Switch>
+        </Router>
     );
 }
 
